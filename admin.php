8e594e2a9b9a8b69613c377cbbaa8fa227263867
Robin MORET <robin.moret@supaero.org>
2022-08-20 10:20:24 +0200

    Simplification de requete

--- admin.php
+++ admin.php
@@ -1360,12 +1360,12 @@ SELECT id,name,uppercats,global_rank
       
  
     $query ='SELECT T1.Price AS price, T1.Shipping AS shipping, T5.Currency AS currency, T10.name AS title, T10.comment AS item, T10.file AS file, MIN(T2.Width_cm) AS minSize_cm, MAX(T2.Width_cm) AS maxSize_cm,'.
-            ' MIN(T2.Width_in) AS minSize_in, MAX(T2.Width_in) AS maxSize_in, T10.path, T7.Material AS item_option, T10.Id AS imageId, T5.CountryCode AS countryISOcode, T12.Id AS categoryId, T9.RatioValue as Ratio, '.
+            ' MIN(T2.Width_in) AS minSize_in, MAX(T2.Width_in) AS maxSize_in, T10.path, T7.Material AS item_option, T10.Id AS imageId, T5.CountryCode AS countryISOcode, T12.Id AS categoryId, T4.RatioValue as Ratio, '.
             ' T12.name AS categoryName, T12.permalink as categoryPL '.
            'FROM '.PPPPP_PRICE_TABLE.' T1 '.
            'CROSS JOIN '.IMAGES_TABLE.' T10 '.
            'LEFT JOIN '.PPPPP_SIZES_TABLE.' T2 ON T1.Size = T2.Id '.
-           'LEFT JOIN '.PPPPP_RATIO_TABLE.' T9 ON T2.Ratio = T9.Id '.
+//           'LEFT JOIN '.PPPPP_RATIO_TABLE.' T9 ON T2.Ratio = T9.Id '.
            'LEFT JOIN '.PPPPP_SUPPORT_TABLE.' T6 ON T1.Support = T6.Id '.
            'LEFT JOIN '.PPPPP_MATERIAL_TABLE.' T7 ON T6.SupportMaterial = T7.Id '.
            'LEFT JOIN '.PPPPP_OPTION_TABLE.' T3 ON T6.SupportOption1 = T3.Id '.
@@ -1529,12 +1529,12 @@ SELECT id,name,uppercats,global_rank
       
  
     $query ='SELECT T10.name AS title, T10.comment AS item, T10.file AS file, MIN(T2.Width_cm) AS minSize_cm, MAX(T2.Width_cm) AS maxSize_cm,'.
-            ' MIN(T2.Width_in) AS minSize_in, MAX(T2.Width_in) AS maxSize_in, T10.path, T7.Material AS item_option, T10.Id AS imageId, T5.CountryLang AS langISOcode, T12.Id AS categoryId, T9.RatioValue as Ratio, '.
+            ' MIN(T2.Width_in) AS minSize_in, MAX(T2.Width_in) AS maxSize_in, T10.path, T7.Material AS item_option, T10.Id AS imageId, T5.CountryLang AS langISOcode, T12.Id AS categoryId, T4.RatioValue as Ratio, '.
             ' T12.name AS categoryName, T12.permalink as categoryPL '.
            'FROM '.PPPPP_PRICE_TABLE.' T1 '.
            'CROSS JOIN '.IMAGES_TABLE.' T10 '.
            'LEFT JOIN '.PPPPP_SIZES_TABLE.' T2 ON T1.Size = T2.Id '.
-           'LEFT JOIN '.PPPPP_RATIO_TABLE.' T9 ON T2.Ratio = T9.Id '.
+  //         'LEFT JOIN '.PPPPP_RATIO_TABLE.' T9 ON T2.Ratio = T9.Id '.
            'LEFT JOIN '.PPPPP_SUPPORT_TABLE.' T6 ON T1.Support = T6.Id '.
            'LEFT JOIN '.PPPPP_MATERIAL_TABLE.' T7 ON T6.SupportMaterial = T7.Id '.
            'LEFT JOIN '.PPPPP_OPTION_TABLE.' T3 ON T6.SupportOption1 = T3.Id '.
